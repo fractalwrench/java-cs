b35ec0561a26b9b8c69df186eff0b05a9ea75273
Jamie <fractalwrench@gmail.com>
2017-02-07 21:06:42 +0000

    initial commit (create stack data structure)

---
+++ src/main/java/com/fractalwrench/structures/Stack.java
@@ -0,0 +1,106 @@
+package com.fractalwrench.structures;
+
+/**
+ * A Java implementation of a Stack data structure.
+ * <p>
+ * Stacks are LIFO
+ *
+ * @param <T> the type of data
+ */
+public class Stack<T> {
+
+    private static final String EMPTY_STACK_MSG = " cannot be called on an Empty Stack!";
+
+    private Node<T> top;
+
+    /**
+     * Removes the top-most object from the stack and returns it
+     * <p>
+     * O(1)
+     *
+     * @return the popped object
+     * @throws IllegalStateException - if no items are in the stack
+     */
+    public T pop() throws IllegalStateException {
+        if (top == null) {
+            throw new IllegalStateException("pop() " + EMPTY_STACK_MSG);
+        } else {
+            T data = top.data;
+            top = top.next;
+            return data;
+        }
+    }
+
+    /**
+     * Adds the given object to the top of the stack
+     * <p>
+     * O(1)
+     *
+     * @param object the given object
+     */
+    public void push(T object) {
+        Node<T> node = new Node<>(object);
+        node.next = top;
+        top = node;
+    }
+
+    /**
+     * Retrieves the top-most object from the stack and returns it
+     * <p>
+     * O(1)
+     *
+     * @return the top-most object
+     */
+    public T peek() {
+        if (top == null) {
+            throw new IllegalStateException("pop() " + EMPTY_STACK_MSG);
+        } else {
+            return top.data;
+        }
+    }
+
+    /**
+     * Determines whether there are any objects in the stack
+     * <p>
+     * O(1)
+     *
+     * @return true if the stack is empty
+     */
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    /**
+     * Determines the number of objects in the stack
+     * <p>
+     * O(n)
+     *
+     * @return the stack's size
+     */
+    public long size() {
+        Node node = top;
+        int count = 0;
+
+        while (node != null) {
+            node = node.next;
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * Holds the data and a pointer to the {@link Node} below.
+     *
+     * @param <T> the data type
+     */
+    private static class Node<T> {
+
+        private final T data;
+        private Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+}
---
+++ src/test/java/com/fractalwrench/structures/StackTest.java
@@ -0,0 +1,87 @@
+package com.fractalwrench.structures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests the {@link Stack} data structure using {@link String}
+ */
+public class StackTest {
+
+    private static final int ELEMENT_COUNT = 10000;
+    private Stack<String> stack;
+
+    @Before
+    public void setUp() throws Exception {
+        stack = new Stack<>();
+    }
+
+    /**
+     * Ensures that the number of elements on the stack is calculated correctly
+     */
+    @Test
+    public void testPopPeekPushSize() throws Exception {
+        assertEquals(0, stack.size());
+        String data = "test";
+        stack.push(null);
+        stack.push(data);
+        assertEquals(2, stack.size());
+
+        assertEquals(data, stack.peek());
+        assertEquals(2, stack.size());
+
+        assertEquals(data, stack.pop());
+        assertEquals(1, stack.size());
+
+        stack.pop();
+        assertEquals(0, stack.size());
+    }
+
+    /**
+     * Ensures that popping an empty stack throws an exception
+     */
+    @Test(expected = IllegalStateException.class)
+    public void emptyPop() throws Exception {
+        stack.pop();
+    }
+
+    /**
+     * Ensures that peeking an empty stack throws an exception
+     */
+    @Test(expected = IllegalStateException.class)
+    public void emptyPeek() throws Exception {
+        stack.peek();
+    }
+
+    /**
+     * Ensures that the stack returns true if it has no objects
+     */
+    @Test
+    public void isEmpty() throws Exception {
+        assertTrue(stack.isEmpty());
+        stack.push("");
+        assertFalse(stack.isEmpty());
+    }
+
+    /**
+     * Tests the stack using a large number of objects
+     */
+    @Test
+    public void testLargeElementUse() {
+        Stack<Integer> integerStack = new Stack<>();
+
+        for (int k = 0; k < ELEMENT_COUNT; k++) {
+            integerStack.push(k);
+        }
+
+        assertEquals(ELEMENT_COUNT, integerStack.size());
+        assertEquals(ELEMENT_COUNT - 1, (int) integerStack.peek());
+        assertEquals(ELEMENT_COUNT - 1, (int) integerStack.pop());
+        assertEquals(ELEMENT_COUNT - 1, (int) integerStack.size());
+    }
+
+}
