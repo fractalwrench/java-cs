1b09dc597c15f059bfbedecf3eda2ac035c81a3e
Jamie <fractalwrench@gmail.com>
2017-02-12 11:46:26 +0000

    pass matrix rotation

--- src/main/java/com/fractalwrench/algorithms/cci/ch1/RotateMatrixSolution.java
+++ src/main/java/com/fractalwrench/algorithms/cci/ch1/RotateMatrixSolution.java
@@ -12,6 +12,44 @@ public class RotateMatrixSolution {
      */
     public void rotateMatrix(int[][] matrix) {
 
+        int len = matrix.length;
+        for (int layer = 0; layer < len / 2; layer++) {
+
+            int first = layer;
+            int end = len - 1 - layer;
+
+            for (int a = first; a< end; a++) {
+                int offset = a - first;
+                int offsetEnd = end - offset;
+
+                // swap the edge of each layer
+                int top = matrix[first][a];
+                matrix[first][a] = matrix[offsetEnd][first];
+                matrix[offsetEnd][first] = matrix[end][end - offset];
+                matrix[end][end - offset] = matrix[a][end];
+                matrix[a][end] = top;
+            }
+        }
+
+
+        // 2nd iter wants   m[0][1] -> m[1][2],
+        //                  m[1][2] -> m[2][1]
+        //                  m[2][1] -> m[1][0]
+        //                  m[1][0] -> m[0][1]
+
+//     {1, 2, 3},
+//     {4, 5, 6},
+//     {7, 8, 9}
+//
+//     {7, 4, 1},
+//     {8, 5, 2},
+//     {9, 6, 3}
+
+        // matrix[0][0] -> matrix[0][n]
+        // matrix[0][n] -> matrix[n][n]
+        // matrix[n][n] -> matrix[n][0]
+        // matrix[0][n] -> matrix[0][0]
+
     }
 
 }
--- src/test/java/com/fractalwrench/algorithms/cci/ch1/RotateMatrixSolutionTest.java
+++ src/test/java/com/fractalwrench/algorithms/cci/ch1/RotateMatrixSolutionTest.java
@@ -38,4 +38,26 @@ public class RotateMatrixSolutionTest {
         assertArrayEquals(expected, input);
     }
 
+    @Test
+    public void largerMatrixRotate() throws Exception {
+        int[][] input = {
+                new int[]{1, 2, 3, 4, 5},
+                new int[]{6, 7, 8, 9, 10},
+                new int[]{11, 12, 13, 14, 15},
+                new int[]{16, 17, 18, 19, 20},
+                new int[]{21, 22, 23, 24, 25}
+        };
+        int[][] expected = {
+                new int[]{21, 16, 11, 6, 1},
+                new int[]{22, 17, 12, 7, 2},
+                new int[]{23, 18, 13, 8, 3},
+                new int[]{24, 19, 14, 9, 4},
+                new int[]{25, 20, 15, 10, 5}
+        };
+
+        solution.rotateMatrix(input);
+        assertArrayEquals(expected, input);
+    }
+
+
 }
